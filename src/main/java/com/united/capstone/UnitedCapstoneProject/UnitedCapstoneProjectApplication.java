93ddd3ccbe9be7a0a726d85e212b36fd8142f6ac
Shrey Jain <shreyjain3245@gmail.com>
2023-03-23 20:34:43 -0400

    Added spring boot setup

---
+++ src/main/java/com/united/capstone/UnitedCapstoneProject/UnitedCapstoneProjectApplication.java
@@ -0,0 +1,12 @@
+package com.united.capstone.UnitedCapstoneProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitedCapstoneProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnitedCapstoneProjectApplication.class, args);
+    }
+}
---
+++ src/test/java/com/united/capstone/UnitedCapstoneProject/UnitedCapstoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.united.capstone.UnitedCapstoneProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitedCapstoneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
